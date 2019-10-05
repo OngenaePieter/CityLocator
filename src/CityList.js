988146b7f3836ba78fec2d2a46913417395b157b
mixxamm <maximjanssensj5@gmail.com>
2019-10-05 12:28:46 +0200

    Voeg zoekbalk toe

--- src/CityList.js
+++ src/CityList.js
@@ -2,12 +2,23 @@ import React from 'react'
 import cities from './cities.json'
 import { propTypes } from 'pigeon-maps'
 
-function CityList(props){
-    return(
-        <ul>{cities.map((city, index) => (
-            <li className={props.selectedCity == city.Name ? 'selected' : null} key={index} onClick={() => props.onChange(city.Lat, city.Lon, city.Name)}>{city.Name}</li>
-        ))}</ul>
-    )
+class CityList extends React.Component{
+    state = {city: null}
+    handleChange = (event) => {
+        this.setState({city: event.target.value});
+      }
+    
+    render(){
+        return(
+            <div>
+            <input type="text" placeholder="Zoek" onChange={this.handleChange}></input>
+            <ul>{cities.map((city, index) => {
+                if(this.state.city === null || city.Name.toLowerCase().includes(this.state.city.toLowerCase()))
+                return (<li className={this.props.selectedCity === city.Name ? 'selected' : null} key={index} onClick={() => this.props.onChange(city.Lat, city.Lon, city.Name)}>{city.Name}</li>)
+            })}</ul>
+            </div>
+        )
+    }
 }
 
 export default CityList
